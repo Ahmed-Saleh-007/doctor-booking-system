76b3291b385d5218e263201e53df601dfa13123a
mohammedegila <mohammedegila@gmail.com>
2021-05-29 20:39:05 +0200

    Add doctor-degree data seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\DoctorDegree;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,40 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        if (DoctorDegree::count() == 0) {
+            $test = [
+                [
+                    'name_en' => 'Professor',
+                    'name_ar' => 'أستاذ',
+                    'description' => 'Professor is an academic rank at universities and other post-secondary education and research institutions in most countries.',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],[
+                    'name_en' => 'Lecturer',
+                    'name_ar' => 'مدرس',
+                    'description' => 'Lecturer is an academic rank within many universities, though the meaning of the term varies somewhat from country to country.',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],[
+                    'name_en' => 'Consultant',
+                    'name_ar' => 'استشاري',
+                    'description' => 'A consultant is a professional who provides advice and further purposeful activities in an area of specialization.',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],[
+                    'name_en' => 'Specialist',
+                    'name_ar' => 'أخصائي',
+                    'description' => 'test Specialist',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ]
+            ];
+            foreach ($test as $t) {
+                DoctorDegree::create($t);
+            }
+        } else {
+            echo "\e[31mTable is not empty, therefore NOT ";
+        }
     }
 }
