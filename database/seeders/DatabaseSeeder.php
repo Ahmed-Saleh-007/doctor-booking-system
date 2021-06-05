1e47c9b9aa38d8eea740eb51e836a623637d37b0
mohammedegila <mohammedegila@gmail.com>
2021-06-05 17:59:35 +0200

    Update Doctor Degree Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,39 +18,5 @@ class DatabaseSeeder extends Seeder
         $this->call(AdminSeeder::class);
         $this->call(SpecialistSeeder::class);
         $this->call(SubSpecialistSeeder::class);
-        if (DoctorDegree::count() == 0) {
-            $test = [
-                [
-                    'name_en' => 'Professor',
-                    'name_ar' => 'أستاذ',
-                    'description' => 'Professor is an academic rank at universities and other post-secondary education and research institutions in most countries.',
-                    'created_at' => date('Y-m-d H:i:s'),
-                    'updated_at' => date('Y-m-d H:i:s'),
-                ],[
-                    'name_en' => 'Lecturer',
-                    'name_ar' => 'مدرس',
-                    'description' => 'Lecturer is an academic rank within many universities, though the meaning of the term varies somewhat from country to country.',
-                    'created_at' => date('Y-m-d H:i:s'),
-                    'updated_at' => date('Y-m-d H:i:s'),
-                ],[
-                    'name_en' => 'Consultant',
-                    'name_ar' => 'استشاري',
-                    'description' => 'A consultant is a professional who provides advice and further purposeful activities in an area of specialization.',
-                    'created_at' => date('Y-m-d H:i:s'),
-                    'updated_at' => date('Y-m-d H:i:s'),
-                ],[
-                    'name_en' => 'Specialist',
-                    'name_ar' => 'أخصائي',
-                    'description' => 'test Specialist',
-                    'created_at' => date('Y-m-d H:i:s'),
-                    'updated_at' => date('Y-m-d H:i:s'),
-                ]
-            ];
-            foreach ($test as $t) {
-                DoctorDegree::create($t);
-            }
-        } else {
-            echo "\e[31mTable is not empty, therefore NOT ";
-        }
     }
 }
