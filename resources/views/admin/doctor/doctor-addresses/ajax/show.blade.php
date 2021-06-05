3ae9bfe7b93bf76555e35d56569fab5761f507d5
mohammedegila <mohammedegila@gmail.com>
2021-06-05 21:26:49 +0200

    update doctor addresses view

--- resources/views/admin/doctor/doctor-addresses/ajax/show.blade.php
+++ resources/views/admin/doctor/doctor-addresses/ajax/show.blade.php
@@ -4,12 +4,14 @@
         <h5 class="card-title">@lang('admin.address_en'):</h5>
         <p class="card-text">{{ $doctor_address->address_en }}</p>
         <h5 class="card-title">@lang('admin.address_ar'):</h5>
-        <p class="card-text">{{ $doctor_address>address_ar }}</p>
+        <p class="card-text">{{ $doctor_address->address_ar }}</p>
         <h5 class="card-title">@lang('doctor.fees'):</h5>
         <p class="card-text">{{ $doctor_address->fees }}</p>
-        <h5 class="card-title">@lang('doctor.fees'):</h5>
-        <p class="card-text">{{ $doctor_address->fees }}</p>
-        <h5 class="card-title">@lang('doctor.fees'):</h5>
-        <p class="card-text">{{ $doctor_address->fees }}</p>
+        <h5 class="card-title">@lang('doctor.name_en'):</h5>
+        <p class="card-text">{{ $doctor_address->doctor->name_en }}</p>
+        <h5 class="card-title">@lang('doctor.name_ar'):</h5>
+        <p class="card-text">{{ $doctor_address->doctor->name_ar }}</p>
+        <h5 class="card-title">@lang('admin.district'):</h5>
+        <p class="card-text">{{ $doctor_address->district->name_en }}</p>
     </div>
 </div>
