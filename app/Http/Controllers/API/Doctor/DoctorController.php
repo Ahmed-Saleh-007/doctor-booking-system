9eca5bafdd5731f302fb3e41a788a36a6d2292c2
mohammedegila <mohammedegila@gmail.com>
2021-06-22 18:12:41 +0200

    Add search Api and district, specialist and name filter

--- app/Http/Controllers/API/Doctor/DoctorController.php
+++ app/Http/Controllers/API/Doctor/DoctorController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\API\Doctor;
 
 use App\Http\Controllers\Controller;
+use App\Models\City;
 use App\Models\Doctor;
+use App\Models\DoctorAddress;
+use App\Models\Specialist;
 use Illuminate\Http\Request;
 
 class DoctorController extends Controller
@@ -14,9 +17,38 @@ class DoctorController extends Controller
         return $doctors;
     }
 
-    function search($key)
+    public function search(Request $request)
     {
-         $doctors = Doctor::where('name_en','like',"%$key%")->get();
-         return $doctors;
+        $doctorDistrictIDs=null;
+        $d=null;
+        $doctor=null;
+
+        if (isset($request->district)) {
+            $doctorDistrictIDs = DoctorAddress::select('doctor_id')->where('district_id', $request->district)->get();
+        }
+
+        if (isset($doctorDistrictIDs)) {
+            $d = Doctor::with(['degree','specialist','subspecialists','addresses','country'])->with(['addresses.doctor_times','addresses.district','addresses.district.city'])
+            ->whereIn('id', $doctorDistrictIDs)->get();
+            $doctor = $d;
+            dd($d);
+        }
+
+        if (isset($request->specialty)) {
+            $doctor =  Doctor::
+            where('spec_id', $request->specialty)->pluck('id', 'name_en')
+            ->with(['degree','specialist','subspecialists','addresses','country'])->with(['addresses.doctor_times','addresses.district','addresses.district.city'])->toArray();
+            if (!isset($d)) {
+                $d = $doctor;
+            }
+        }
+
+        // if (isset($doctor) && isset($d)) {
+        //     $result = array_intersect($d, $doctor);
+        // } else {
+        //     $result = "No data";
+        // }
+
+        return $d ;
     }
 }
---
+++ app/Http/Controllers/API/Doctor/SpecialistController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\API\Doctor;
+
+use App\Http\Controllers\Controller;
+use App\Models\Specialist;
+use Illuminate\Http\Request;
+
+class SpecialistController extends Controller
+{
+    public function index()
+    {
+        $specialist = Specialist::select('id', 'name_en')->paginate(5);
+        return $specialist;
+    }
+}
